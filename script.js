a444f67de6fef0fd0f4d5d4560ac8f52b8b55b18
Olena <olena.tudoi@gmail.com>
2022-09-02 19:47:24 -0500

    Added delete function

--- script.js
+++ script.js
@@ -11,7 +11,9 @@ class Calculator {
     this.operation = undefined;
   }
 
-  delete() {}
+  delete() {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1);
+  }
 
   appendNumber(number) {
     if (this.currentOperand === '.' && this.currentOperand.includes('.'))
@@ -55,9 +57,36 @@ class Calculator {
     this.previousOperand = '';
   }
 
+  getDisplayNumber(number) {
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split('.')[0]);
+    const decimalDigits = stringNumber.split('.')[1];
+    let integerDisplay;
+    if (isNaN(integerDigits)) {
+      integerDisplay = '';
+    } else {
+      integerDisplay = integerDigits.toLocaleString('en', {
+        maximumFractionDigits: 0,
+      });
+    }
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`;
+    } else {
+      return integerDisplay;
+    }
+  }
+
   updateDisplay() {
-    this.currentOperandTextElement.innerText = this.currentOperand;
-    this.previousOperandTextElement.innerText = this.previousOperand;
+    this.currentOperandTextElement.innerText = this.getDisplayNumber(
+      this.currentOperand
+    );
+    if (this.operation != null) {
+      this.previousOperandTextElement.innerText = `${this.getDisplayNumber(
+        this.previousOperand
+      )} ${this.operation}`;
+    } else {
+      this.previousOperandTextElement.innerText = '';
+    }
   }
 }
 
@@ -95,5 +124,10 @@ equalsButton.addEventListener('click', () => {
 
 allClearButton.addEventListener('click', () => {
   calculator.clear();
+  updateDisplay();
+});
+
+deleteButton.addEventListener('click', () => {
+  calculator.delete();
   updateDisplay();
 });
